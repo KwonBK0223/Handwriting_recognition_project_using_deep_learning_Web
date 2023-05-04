b80025d25115085cc823f6648811d10a86a6e529
KwonBK0223 <114387888+KwonBK0223@users.noreply.github.com>
2023-05-05 02:05:55 +0900

    web.py

--- web.py
+++ web.py
@@ -20,59 +20,38 @@ def load():
 # 알파벳 대문자 레이블
 labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-# 홈 페이지
-def home():
-    CANVAS_SIZE = 192
+CANVAS_SIZE = 192
 
-    col1, col2 = st.columns(2)
+col1, col2 = st.columns(2)
 
-    with col1:
-        canvas = st_canvas(
-            fill_color='#000000',
-            stroke_width=12,
-            stroke_color='#FFFFFF',
-            background_color='#000000',
-            width=CANVAS_SIZE,
-            height=CANVAS_SIZE,
-            drawing_mode='freedraw',
-            key='canvas'
-        )
+with col1:
+    canvas = st_canvas(
+        fill_color='#000000',
+        stroke_width=12,
+        stroke_color='#FFFFFF',
+        background_color='#000000',
+        width=CANVAS_SIZE,
+        height=CANVAS_SIZE,
+        drawing_mode='freedraw',
+        key='canvas'
+    )
 
-    if canvas.image_data is not None:
-        img = canvas.image_data.astype(np.uint8)
-        img = cv2.resize(img, dsize=(28, 28))
-        preview_img = cv2.resize(img, dsize=(CANVAS_SIZE, CANVAS_SIZE), interpolation=cv2.INTER_NEAREST)
+if canvas.image_data is not None:
+    img = canvas.image_data.astype(np.uint8)
+    img = cv2.resize(img, dsize=(28, 28))
+    preview_img = cv2.resize(img, dsize=(CANVAS_SIZE, CANVAS_SIZE), interpolation=cv2.INTER_NEAREST)
 
-        col2.image(preview_img)
+    col2.image(preview_img)
 
-        x = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        x = x.reshape((-1, 28, 28, 1))
-        y = model.predict(x).squeeze()
+    x = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    x = x.reshape((-1, 28, 28, 1))
+    y = model.predict(x).squeeze()
 
-    # 예측 결과의 최댓값 인덱스를 구함
-    pred_idx = np.argmax(y)
-    # 레이블에 해당하는 문자를 가져옴
-    pred_char = labels[pred_idx]
-    
-    st.write('## Prediction : %s' % pred_char)
-    st.bar_chart(y)
+# 예측 결과의 최댓값 인덱스를 구함
+pred_idx = np.argmax(y)
+# 레이블에 해당하는 문자를 가져옴
+pred_char = labels[pred_idx]
 
-# 두 번째 페이지
-def page1():
-    st.write("이것은 두 번째 페이지입니다.")
+st.write('## Prediction : %s' % pred_char)
+st.bar_chart(y)
 
-# 세 번째 페이지
-def page2():
-    st.write("이것은 세 번째 페이지입니다.")
-
-# 메뉴 생성
-menu = ['Prediction', 'What is CNN', 'Code']
-choice = st.selectbox("메뉴", menu)
-
-# 메뉴에 따른 페이지 선택
-if choice == 'Prediction':
-    home()
-elif choice == 'What is CNN':
-    page1()
-elif choice == 'Code':
-    page2()
